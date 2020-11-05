e8ea88c630a8416b4f24fa6c9327fe554671bb68
derpyninja <felix.zaussinger@gmail.com>
2020-11-05 18:31:40 +0100

    implemented simple map class

---
+++ src/gecm/dicts.py
@@ -0,0 +1,80 @@
+import numpy as np
+from copy import deepcopy
+
+
+# implements the detailed NFI mapping
+nfi_mapping = {
+    "Bare area": 1,
+    "Agriculture land": 2,
+    "Urban": 3,
+    "Grassland": 4,
+    "Quarry": 5,
+    "Road": 6,
+    "Other vegetation": 7,
+    "River": 8,
+    "Open water": 9,
+    "Windfarm": 10,
+    "Assumed woodland": 11,
+    "Broadleaved": 12,
+    "Conifer": 13,
+    "Felled": 14,
+    "Failed": 15,
+    "Ground prep": 16,
+    "Low density": 17,
+    "Mixed mainly broadleaved": 18,
+    "Mixed mainly conifer": 19,
+    "Young trees": 20,
+    "Coppice with standards": 21,
+    "Shrub": 22,
+    "Windblow": 23,
+}
+
+
+def invert_dict(d):
+    """
+    Invert dictionary.
+
+    Parameters
+    ----------
+    d : dict
+        python dictionary
+
+    Returns
+    -------
+    dict
+        Inverted python dictionary
+    """
+    return dict((v, k) for k, v in d.items())
+
+
+def int2class(int_array, max_string_length=40):
+    """
+    Map integers to LULC classes.
+
+    Parameters
+    ----------
+    int_array : np.array (int)
+        2D Array representing a LULC map via integers
+
+    Returns
+    -------
+    np.array (str)
+        2D Array representing a LULC map via classes
+    """
+    class_array = deepcopy(int_array).astype("S{}".format(max_string_length))
+    boolean_mask = int_array.mask
+    inverse_nfi_mapping = invert_dict(nfi_mapping)
+
+    # does work for np.ma
+    if int_array.ndim > 1:
+        # re-map
+        for (index, val) in np.ndenumerate(int_array):
+            masked = boolean_mask[index]
+            if not masked:
+                class_array[index[0], index[1]] = inverse_nfi_mapping[val]
+
+        return class_array
+
+    else:
+        # currently does not work for np.ma
+        return np.array([inverse_nfi_mapping[i] for i in int_array])
---
+++ src/gecm/field.py
@@ -0,0 +1,112 @@
+import os
+import numpy as np
+import rasterio as rio
+import matplotlib.pyplot as plt
+from rasterio.plot import show, show_hist
+from src.gecm.dicts import nfi_mapping, int2class
+
+
+class Map(object):
+    """
+    Implements playing field class.
+    """
+
+    def __init__(self, fpath):
+        self.fpath = fpath
+        self.src = rio.open(self.fpath)
+        self.rows = self.src.width
+        self.cols = self.src.height
+        self.lulc_mapping = nfi_mapping
+
+    def read(self, masked=True):
+        """
+        Read geotiff via rasterio.
+
+        Parameters
+        ----------
+        masked : bool
+            Whether to parse as np.ma
+
+        Returns
+        -------
+        np.ma
+            Parsed LULC map
+        """
+        return self.src.read(1, masked=masked)
+
+    def convert(self):
+        """
+        Convert from integer to LULC representation.
+
+        Returns
+        -------
+        np.ma
+            Masked array of LULC strings
+        """
+        return int2class(self.read())
+
+    def show(self):
+        """
+        Create a spatial plot of the map.
+
+        Returns
+        -------
+        ax :
+            matplotlib ax object
+        """
+        # read
+        raster = self.read()
+
+        # plot
+        fig, ax = plt.subplots()
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_title("Map size: {} x {}".format(self.rows, self.cols))
+
+        show(raster, ax=ax, transform=self.src.transform, cmap="Paired")
+        plt.tight_layout()
+        return ax
+
+    def show_barh(self):
+        """
+        Create a barplot of the current distribution of areal
+        percentage for all LULC types.
+
+        Returns
+        -------
+        ax :
+            matplotlib ax object
+        """
+        raster = self.read()
+
+        # get unique value counts
+        unique, counts = np.unique(raster, return_counts=True)
+
+        # bar width as percent of total pixels ~ areal percentage
+        bar_width = counts[:-1] / (self.rows * self.cols)
+
+        # create barplot
+        fig, ax = plt.subplots()
+        ax.barh(y=int2class(unique[:-1]), width=bar_width)
+        ax.set_xlabel("Percent of total area (%)")
+        plt.tight_layout()
+        return ax
+
+
+if __name__ == "__main__":
+
+    # define dirs
+    data_raw = os.path.join("..", "..", "data", "raw")
+    data_processed = os.path.join("..", "..", "data", "processed")
+    figure_dir = os.path.join("..", "..", "plots")
+
+    # load map
+    fpath_map = os.path.join(data_processed, "NFI_rasterized_40_40.tif")
+    field = Map(fpath=fpath_map)
+
+    # create plot
+    field.show()
+    field.show_barh()
+
+    # show plots
+    plt.show()
